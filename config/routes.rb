1b4e2c50a959468d19dc34742bd4c5d366d6a549
Ahmed MoYasin <ahmed.mo.yasin@gmail.com>
2016-11-19 15:15:27 +0000

    Created the About page and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/home'
   
+  get 'pages/about'
+  
   root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
